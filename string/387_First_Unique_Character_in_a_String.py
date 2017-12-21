c5ef24da1ee1f6b32a8443d45eba9c67329830f3
pureoym <pureoym@163.com>
2017-12-21 15:56:39 +0800

    test

--- string/387_First_Unique_Character_in_a_String.py
+++ string/387_First_Unique_Character_in_a_String.py
@@ -44,6 +44,7 @@ if __name__ == "__main__":
     """
     Given a string, find the first non-repeating character in it and return
     it's index. If it doesn't exist, return -1.
+    寻找字符串中第一次仅出现过一次的字符的下标，没有返回-1.
     Examples:
         s = "leetcode", return 0.
         s = "loveleetcode", return 2.
